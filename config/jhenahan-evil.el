06a95f3939ae91b31fb15f386c06ff2bba0d138b
Jack Henahan <jhenahan@uvm.edu>
2013-10-10 10:59:50 -0400

    fix evil-surround

--- config/jhenahan-evil.el
+++ config/jhenahan-evil.el
@@ -13,14 +13,20 @@
 (evil-mode 1)
 (setq evil-default-cursor t)
 
+(global-surround-mode)
+(add-to-list 'surround-operator-alist
+             '(evil-paredit-change . change))
+(add-to-list 'surround-operator-alist
+             '(evil-paredit-delete . delete))
 (add-hook 'prog-mode-hook 'paredit-mode)
 (add-hook 'prog-mode-hook 'evil-paredit-mode)
 (add-hook 'text-mode-hook 'paredit-mode)
 (add-hook 'text-mode-hook 'evil-paredit-mode)
-(global-surround-mode)
 
 (define-key evil-ex-map "e " 'ido-find-file)
 (define-key evil-ex-map "w " 'ido-write-file)
 (define-key evil-ex-map "b " 'ido-switch-buffer)
+
+(global-set-key (kbd "C-a") 'evil-numbers/inc-at-pt)
 
 (provide 'jhenahan-evil)
