bfda59d81c7a57c5cc1634fe22d51a99e854da77
Jack Henahan <jhenahan@uvm.edu>
2013-10-10 10:56:53 -0400

    add emacsclient compat and theme-changer

--- config/jhenahan-ui.el
+++ config/jhenahan-ui.el
@@ -1,8 +1,17 @@
-(defadvice load-theme
-  (before theme-dont-propagate activate)
-  (mapcar #'disable-theme custom-enabled-themes))
-(load-theme 'graham)
-(set-face-attribute 'default nil
-                :family "DejaVu Sans Mono" :height 140)
+(defadvice load-theme  
+  (before theme-dont-propagate activate) 
+  (mapc #'disable-theme custom-enabled-themes))
+(setq calendar-location-name "Burlington, VT")
+(setq calendar-latitude 44.48)
+(setq calendar-longitude -73.21)
+
+(require 'theme-changer)
+(change-theme 'solarized-light 'solarized-dark)
+
+
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-14"))
 
 (provide 'jhenahan-ui)
