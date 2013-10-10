9633226804ac672844949e2da99d43aa4bff5356
Jack Henahan <jhenahan@uvm.edu>
2013-10-10 11:00:59 -0400

    global flycheck

--- config/jhenahan-flycheck.el
+++ config/jhenahan-flycheck.el
@@ -1,5 +1,5 @@
 (require 'flycheck-color-mode-line)
-
+(add-hook 'after-init-hook 'global-flycheck-mode)
 (eval-after-load "flycheck"
   '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
 
